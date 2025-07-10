c18882a0811540d30a93ef05ab1e885c07fb254a
Nitesh kumar <nk41521391@gmail.com>
2025-07-10 19:37:50 +0530

    new file added

--- Binary Tree/Main.java
+++ Binary Tree/Main.java
@@ -16,70 +16,51 @@ class BinaryTree {
     Node root;
     // Function to change the tree to hold the children sum property here
     void convertTree(Node node) {
-        int left_data = 0, right_data = 0, diff;
-
-        // If the leaf node or the tree is empty then return true here
+        
         if (node == null || (node.left == null && node.right == null)) {
             return;
-        } else {
-            // Convert the left and the right subtrees
-            convertTree(node.left);
-            convertTree(node.right);
+        } 
 
-            // If the left child is not present then 0 is used as data for the left child
-            if (node.left != null) {
-                left_data = node.left.data;
-            }
-
-            // If the right child is not present in the tree then 0 is used as data value of right child in the tree
-            if (node.right != null) {
-                right_data = node.right.data;
-            }
-
-            // Get the diff value  of the node's data and children sum
-            diff = left_data + right_data - node.data;
-
-            // If a node's children sum is greater than the node's data
-            if (diff > 0) {
-                node.data = node.data + diff;
-            }
-
-            // If node's data value is greater than the children's sum, then increment subtree by diff
-            if (diff < 0)
-                // -diff is used to make diff positive
-                increment(node, -diff);
+        int left_data = node.left != null? node.left.data : 0;
+        int right_data = node.right != null ? node.right.data : 0;
+        
+        if(node.data > left_data + right_data){
+            if(node.left!=null)
+                node.left.data = node.data;
+            if(node.right!=null)
+                node.right.data = node.data;
         }
-    }
 
-    // Function to increment subtree by diff
-    void increment(Node node, int diff) {
-        // IF left child is not NULL then increment it
-        if (node.left != null) {
-            node.left.data = node.left.data + diff;
-
-            // Recursively call to fix the descendants of node->left
-            increment(node.left, diff);
-        } else if (node.right != null) // Else increment right child
-        {
-            node.right.data = node.right.data + diff;
-            // Recursively call to fix the descendants of node->right
-            increment(node.right, diff);
+        if(node.data < left_data + right_data){
+            node.data = left_data + right_data;
         }
+
+        convertTree(node.left);
+        convertTree(node.right);
+
+        int node_data = 0;
+        if(node.left != null){
+            node_data += node.left.data;
+        }
+        if(node.right != null){
+            node_data += node.right.data;
+        }
+        node.data = node_data;
     }
 
     // Given a binary tree, print the inorder traversal using the function printInorder()
-    void printInorder(Node node) {
+    void printPreorder(Node node) {
         if (node == null)
             return;
-
-        // First recur on left child
-        printInorder(node.left);
-
+        
         // Then print the data of node
         System.out.print(node.data + " ");
 
+        // First recur on left child
+        printPreorder(node.left);
+
         // Now recur on right child
-        printInorder(node.right);
+        printPreorder(node.right);
     }
 
 }
@@ -97,13 +78,13 @@ class Main {
         tree.root.right.right = new Node(2);
 
         System.out.println("Inorder traversal before conversion is :");
-        tree.printInorder(tree.root);
+        tree.printPreorder(tree.root);
 
         tree.convertTree(tree.root);
-        System.out.println("");
+        System.out.println();
 
         System.out.println("Inorder traversal after conversion is :");
-        tree.printInorder(tree.root);
+        tree.printPreorder(tree.root);
 
     }
 }
