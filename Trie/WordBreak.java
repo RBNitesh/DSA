154dda01e52e6302652309ccf4d6e628e8d69365
Nitesh Kumar <nk41521391@gmail.com>
2026-01-14 15:05:16 +0530

    new file

---
+++ Trie/WordBreak.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        
+    }
+}
+
+public class WordBreak {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        List<String> wordDict = new ArrayList<>();
+        String s = sc.nextLine();
+
+        
+    }
+}
