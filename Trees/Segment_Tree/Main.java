1fa2f5d1c9f3d27d09c4035b82fefff39d585d9d
Nitesh Kumar <nk41521391@gmail.com>
2026-02-07 19:31:10 +0530

    file has been modified

--- Trees/Segment_Tree/Main.java
+++ Trees/Segment_Tree/Main.java
@@ -1,37 +1,57 @@
 import java.util.*;
-class SegmentTree{
-    public int[] createSegmentTree(int[] a){
-        int n = a.length;
-        int[] segTree = new int[n];
-        Arrays.fill(segTree, Integer.MIN_VALUE);
 
-        for(int i=n-1; i>=0; --i){
-            updateTree(segTree, a[i], i);
-        }
-        return segTree;
+class SegmentTree {
+
+    void SegmentTree(int n) {
     }
 
-    public void updateTree(int[] segTree, int val, int i){
-        segTree[i] = val;
-
-        while(i >= 0){
-            int parent = (i-1) >> 1;
-            segTree[parent] += segTree[i];
-
-            i = parent;
+    void buildTree(int i, int l, int r, int[] arr, int[] sgt) {
+        if (l == r) {
+            sgt[i] = arr[r];
+            return;
         }
+         
+        int mid = (l + r) >> 1;
+        
+        buildTree(2 * i + 1, l, mid, arr, sgt);
+        buildTree(2 * i + 2, mid + 1, r, arr, sgt);
+        
+        sgt[i] = sgt[2 * i + 1] + sgt[2 * i + 2];
     }
 }
 
 public class Main{
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5,6,7};
+        int[] arr = { 1, 2, 3, 4, 5, 6, 7 };
 
-        SegmentTree Tree = new SegmentTree();
+        SegmentTree obj = new SegmentTree();
+        int[] sgt = new int[2 * arr.length];
+        obj.buildTree(0, 0, arr.length - 1, arr, sgt);
 
-        int[] segTree = Tree.createSegmentTree(arr);
-        for(int mx : segTree){
-            System.out.print(mx + " ");
+        // for (int sum : sgt) {
+            // System.out.print(sum + " ");
+        // }
+ 
+        System.out.println(findSum(2, 3, 0, arr.length - 1, 0, arr, sgt));
+        System.out.println(findSum(1, 5, 0, arr.length - 1, 0, arr, sgt));
+    }
+    
+    public static int findSum(int l1, int r1, int l2, int r2, int i, int[] arr, int[] sgt) {
+        if (l1 == l2 && r1 == r2) {
+            return sgt[i];
         }
+
+        int mid = (l2 + r2) >> 1;
+
+        int sum = 0;
+        
+        if (l1 <= mid) {
+            sum += findSum(l1, Math.min(r1, mid), l2, mid, 2 * i + 1, arr, sgt);
+        }
+        if (r1 > mid) {
+            sum += findSum(Math.max(l1, mid + 1), r1, mid + 1, r2, 2 * i + 2, arr, sgt);
+        }
+        
+        return sum;
     }
 }
