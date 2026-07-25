0d048c3f42140007be46bb7cad7e6d6275fb8615
Nitesh Kumar <nk41521391@gmail.com>
2026-07-25 21:07:49 +0530

    all changes are saved

---
+++ CSES/AdvandeTechniques/Cut_and_Paste.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+using vi = vector<int>;
+using vl = vector<ll>;
+using vb = vector<bool>;
+using vs = vector<string>;
+using vpi = vector<pair<int, int>>;
+using vpl = vector<pair<ll, ll>>;
+using vvi = vector<vector<int>>;
+using vvl = vector<vector<ll>>;
+using vvb = vector<vector<bool>>;
+using vvs = vector<vector<string>>;
+using vvpi = vector<vector<pair<int, int>>>;
+using vvpl = vector<vector<pair<ll, ll>>>;
+
+ll M = 1e9 + 7;
+ll INF1 = INT_MAX;
+ll INF2 = LLONG_MAX;
+
+#define f(i, a, b) for (int i = a; i <= b; ++i)
+#define F(i, a, b, k) for (int i = a; i <= b; i += k)
+#define all(x) (x).begin(), (x).end()
+#define si(x) scanf("%d", &x)
+
+// optimizes bitwise operations
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("popcnt")
+
+void solve(){
+    
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    solve();
+}
